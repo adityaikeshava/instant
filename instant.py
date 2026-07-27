e1af547bff4cb976fc27e7bdba0b74518413438c
Aditya <aditya.i.keshava@gmail.com>
2026-07-27 13:29:05 +0530

    Update instant.py

--- instant.py
+++ instant.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
-
+from openai import OpenAI
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 
-@app.get("/api", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 def instant():
     message = "Live from production after deploying to vercel"
     client = OpenAI()
     messages = [{"role": "user", "content": message}]
     response = client.chat.completions.create(model="gpt-5-nano", messages=messages)
     reply = response.choices[0].message.content
-    html = f"<html><head><title>Live in an Instant</title></head><body><h1>{reply}</h1></body></html>"
+    html = {
+         f"<html><head><title>Live in an Instant</title></head><body><h1>{reply}</h1></body></html>"
+    }
     return html
+    
