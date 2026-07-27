97c165c3e2d59b13fb06bd2ff6da50cda08f088d
Aditya <aditya.i.keshava@gmail.com>
2026-07-27 13:19:58 +0530

    Using OpenAI API

--- instant.py
+++ instant.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI  # type: ignore
+from fastapi.responses import PlainTextResponse  # type: ignore
+from openai import OpenAI  # type: ignore
 
-app  = FastAPI()
-@app.get("/")
+app = FastAPI()
+
+@app.get("/api", response_class=HTMLResponse)
 def instant():
-    return "Live from production!"
+    message = "Live from production after deploying to vercel"
+    client = OpenAI()
+    messages = [{"role": "user", "content": message}]
+    response = client.chat.completions.create(model="gpt-5-nano", messages=messages)
+    reply = response.choices[0].message.content
+    html = f"<html><head><title>Live in an Instant</title></head><body><h1>{reply}</h1></body></html>"
+    return html
